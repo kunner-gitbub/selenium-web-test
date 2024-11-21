7d92314e317284c487d63e53cba997658c2137cc
kunner-gitbub <kunner.jiang@gmail.com>
2024-11-21 12:03:37 -0500

    MethodTest

---
+++ src/test/java/com/kun/tests/MethodTest.java
@@ -0,0 +1,52 @@
+package com.kun.tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * ClassName: SeperateMethodTest
+ * Package: com.kun.tests
+ * Description:
+ *
+ * @Author KunJiang
+ * @Create 11/21/24 11:52 AM
+ * @Version 1.0
+ */
+
+public class MethodTest {
+    static WebDriver driver;
+    @Test
+    public void test(){
+        setBrowserConfig();
+        runTest();
+        System.out.println("*****second test*********");
+    }
+    public static void setBrowserConfig(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+    public static void runTest(){
+        try {
+            // Open Google
+            driver.get("https://www.google.com");
+
+            // Find search box, enter a query, and submit
+            WebElement searchBox = driver.findElement(By.name("q"));
+            searchBox.sendKeys("Selenium WebDriver");
+            searchBox.submit();
+
+            // Wait for results and print page title
+            Thread.sleep(2000); // Not recommended for production
+            System.out.println("Page title is: " + driver.getTitle());
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // Close the browser
+            driver.quit();
+        }
+    }
+}
