d658549fd3fcef13903344af17e70acd066b7afd
kunner-gitbub <kunner.jiang@gmail.com>
2024-11-22 16:07:16 -0500

    Browser

---
+++ src/test/java/com/kun/tests/TestClass1.java
@@ -0,0 +1,21 @@
+package com.kun.tests;
+
+/**
+ * ClassName: TestClass1
+ * Package: com.kun.tests
+ * Description:
+ *
+ * @Author KunJiang
+ * @Create 11/22/24 3:46 PM
+ * @Version 1.0
+ */
+
+import org.testng.annotations.Test;
+
+public class TestClass1 {
+    @Test
+    public void testMethod1() {
+        System.out.println("TestClass1 - testMethod1");
+    }
+}
+
---
+++ src/test/java/com/kun/tests/TestClass2.java
@@ -0,0 +1,20 @@
+package com.kun.tests;
+
+/**
+ * ClassName: TestClass2
+ * Package: com.kun.tests
+ * Description:
+ *
+ * @Author KunJiang
+ * @Create 11/22/24 3:46 PM
+ * @Version 1.0
+ */
+
+import org.testng.annotations.Test;
+
+public class TestClass2 {
+    @Test
+    public void testMethod2() {
+        System.out.println("TestClass2 - testMethod2");
+    }
+}
